5477a9e1077e26d795433dbcda24c436ab40867f
Shaquille <34082139+shaqiru@users.noreply.github.com>
2020-11-29 13:34:10 +0700

    Create DS18b20_ESP32.ino

---
+++ DS18b20_ESP32.ino
@@ -0,0 +1,48 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const int oneWireBus = 4;     
+
+OneWire oneWire(oneWireBus);
+
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+  delay(5000);
+}
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const int oneWireBus = 4;     
+
+OneWire oneWire(oneWireBus);
+
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+  delay(5000);
+}
